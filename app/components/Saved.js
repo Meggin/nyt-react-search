0a76571901b5153c736aed0aa47ea20b4d9d8f89
Margaret Mary Kearney <mkearney@google.com>
2017-06-14 06:31:17 -0700

    starter components

--- app/components/Saved.js
+++ app/components/Saved.js
@@ -3,11 +3,22 @@ var React = require("react");
 
 // Create the Saved component
 var Saved = React.createClass({
-  render: function() {
-    return (
-      <h1>I'm the Saved Component!!</h1>
-    );
-  }
+  	render: function() {
+    	return (
+			<div>
+	          	<div className="row">
+	  				<div className="col-sm-12">
+	    				<div className="panel panel-primary">
+	      					<div className="panel-heading">
+	        					<h3 className="panel-title">Saved Articles</h3>
+	      					</div>
+	      					<div className="panel-body" id="well-section"></div>
+	    				</div>
+	  				</div>
+			  	</div>
+			</div>		
+    	);
+  	}
 });
 
 // Export the component back for use in other files
--- app/components/Search.js
+++ app/components/Search.js
@@ -3,42 +3,48 @@ var React = require("react");
 
 // Create the Search component.
 var Search = React.createClass({
-  render: function() {
-    return (
-              <div className="panel panel-primary">
-                <div className="panel-heading">
-                  <h3 className="panel-title">Search Parameters</h3>
-                </div>
-                <div className="panel-body">
-                  <form role="form">
-                    <div className="form-group">
-                      <label for="search">Search Term:</label>
-                      {/*<input type="text" class="form-control" id="search-term">*/}
-                    </div>
-                    <div className="form-group">
-                      <label for="pwd">Number of Records to Retrieve:</label>
-                      <select class="form-control" id="num-records-select">
-						<option value="1">1</option>
-						<option value="5" selected>5</option>
-						<option value="10">10</option>
-      				</select>
-                    </div>
-                    <div className="form-group">
-                      <label for="start-year">Start Year (Optional):</label>
-                      {/*<input type="text" class="form-control" id="start-year">*/}
-                    </div>
-                    <div classNBame="form-group">
-                      <label for="end-year">End Year (Optional):</label>
-                      {/*<input type="text" class="form-control" id="end-year">*/}
-                    </div>
-                    <button type="submit" className="btn btn-default" id="run-search">Search</button>
-                    <button type="button" className="btn btn-default" id="clear-all">Clear Results</button>
-
-                  </form>
-                </div>
-              </div>
-    );
-  }
+	render: function() {
+		return (
+			<div>
+		    	<div className="row">
+						<div className="col-sm-12">
+						<div className="panel panel-primary">
+		        			<div className="panel-heading">
+		          				<h3 className="panel-title">Search</h3>
+		        			</div>
+		        			<div className="panel-body">
+		          				<form role="form">
+		            				<div className="form-group">
+		              					<label for="search">Topic</label>
+		              					{/*<input type="text" class="form-control" id="search-term">*/}
+		            				</div>
+		            				<div className="form-group">
+		             					<label for="start-year">Start Year</label>
+		              					{/*<input type="text" class="form-control" id="start-year">*/}
+		            				</div>
+		            				<div className="form-group">
+		             	 				<label for="end-year">End Year</label>
+		              					{/*<input type="text" class="form-control" id="end-year">*/}
+		            				</div>
+		            				<button type="submit" className="btn btn-default" id="run-search">Search</button>
+		          				</form>
+		        			</div>
+		      			</div>
+					</div>
+				</div>
+		      	<div className="row">
+						<div className="col-sm-12">
+						<div className="panel panel-primary">
+		  					<div className="panel-heading">
+		    					<h3 className="panel-title">Results</h3>
+		  					</div>
+		  					<div className="panel-body" id="well-section"></div>
+						</div>
+						</div>
+			  	</div>
+			</div>
+    	);
+  	}
 });
 
 // Export the component back for use in other files
